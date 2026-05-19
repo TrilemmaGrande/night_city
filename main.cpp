cc1b00a043a252993b3f0afa433d8d090e74cf80
trilemma <psychokater@protonmail.com>
2026-05-19 21:29:56 +0200

    fix: Performance, fake trail

--- main.cpp
+++ main.cpp
@@ -13,21 +13,19 @@ int main() {
     std::vector<Building> buildings;
     World::generateWorld(buildings, cars, cfg);
     Camera3D camera = World::initCamera();
-    int screenWidth = 1200;
-    int screenHeight = 1024;
+    constexpr int screenWidth = 1200;
+    constexpr int screenHeight = 1024;
     InitWindow(screenWidth, screenHeight, "Night City");
-
-
     DisableCursor();
     SetTargetFPS(144);
 
     while (!WindowShouldClose()) {
-        UpdateCamera(&camera, CAMERA_ORBITAL);
-        BeginDrawing();
-        ClearBackground(BLACK);
         for (auto &car: cars) {
             car.move(cfg.spawnRadius);
         }
+        UpdateCamera(&camera, CAMERA_ORBITAL);
+        BeginDrawing();
+        ClearBackground(BLACK);
         BeginMode3D(camera);
         DrawPlane({0, 0, 0}, {50, 50}, Color{0, 10, 10, 255});
         for (auto &car: cars) {
